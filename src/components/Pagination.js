22ff9d442cb85bbcc60eb6b39c639e120ed5bee4
Nickolay Chernyi <s0fk@ya.ru>
2019-09-16 15:29:54 +0300

    paggination almost fixed

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,13 +2,12 @@ import React from "react";
 import { fetchMovies } from "../actions/moviesActions";
 import { connect } from "react-redux";
 
-function Pagination({page}, {pages}) {
+function Pagination({page, pages, dispatch}) {
     return (
       <div className="pagination">
         Page {page} of {pages}
-        {/*
-        <button onClick={() => this.props.dispatch(fetchMovies(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=${page + 1}`))}>NExt</button>
-        */}
+        {(page > 1) ? <button onClick={() => dispatch(fetchMovies(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=${page - 1}`))}>Previous</button> : ''}
+        {(page < pages) ? <button onClick={() => dispatch(fetchMovies(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=${page + 1}`))}>Next</button> : ''}
       </div>
     )
   };
